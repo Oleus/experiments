1f40434dcab0b0512a3cd5c426b138e2b1a3e6b1
Olga Leus <olga.leus@siliconmint.com>
2014-11-26 16:57:35 +0300

    parse tweets

---
+++ twitparser/twitparser.spark/src/main/java/twitparser/spark/Constants.java
@@ -0,0 +1,11 @@
+package twitparser.spark;
+
+public final class Constants {
+    public static String ConsumerKey = "";
+    public static String ConsumerSecret = "";
+    public static String AccessToken = "";
+    public static String AccessTokenSecret = "";
+
+    public static String PositiveRatingMask = ".*(good|great|awesome|cool|honor|right|respect|benefi|adept|expert|safe|positive).*";
+    public static String NegativeRatingMask = ".*(bad|terrible|awful|fear|negat|rubber|poor|unfavo|unsuit|horri|uncomfort|unfit|harm|evil).*";
+}
--- twitparser/twitparser.spark/src/main/java/twitparser/spark/SparkStreamingApplication.java
+++ twitparser/twitparser.spark/src/main/java/twitparser/spark/SparkStreamingApplication.java
@@ -1,16 +1,18 @@
 package twitparser.spark;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.PairFlatMapFunction;
 import org.apache.spark.streaming.Duration;
-import org.apache.spark.streaming.api.java.JavaDStream;
-import org.apache.spark.streaming.api.java.JavaReceiverInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.twitter.TwitterUtils;
+import scala.Tuple2;
 import twitter4j.Status;
+import twitter4j.auth.OAuthAuthorization;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class SparkStreamingApplication {
@@ -20,14 +22,37 @@ public class SparkStreamingApplication {
 
         SparkConf config = new SparkConf().setMaster("local[2]").setAppName("Twitparser");
         JavaStreamingContext context = new JavaStreamingContext(config, new Duration(1000));
+        Configuration twitterConfig = new ConfigurationBuilder()
+                .setJSONStoreEnabled(true)
+                .setOAuthConsumerKey(Constants.ConsumerKey)
+                .setOAuthConsumerSecret(Constants.ConsumerSecret)
+                .setOAuthAccessToken(Constants.AccessToken)
+                .setOAuthAccessTokenSecret(Constants.AccessTokenSecret)
+                .build();
 
-        TwitterUtils.createStream(context)
+        TwitterUtils.createStream(context, new OAuthAuthorization(twitterConfig), keyWords)
                 .map(new Function<Status, String>() {
                          @Override
                          public String call(Status status) throws Exception {
-                             return status.getText();
+                             return status.getText().toLowerCase();
                          }
                      }
-                );
+                )
+                .flatMapToPair(new PairFlatMapFunction<String, String, String>() {
+                    @Override
+                    public Iterable<Tuple2<String, String>> call(String tweet) throws Exception {
+                        ArrayList<Tuple2<String, String>> result = new ArrayList<Tuple2<String, String>>();
+
+                        for (String keyWord : keyWords) {
+                            if (tweet.contains(keyWord)) {
+                                result.add(new Tuple2<String, String>(keyWord, tweet));
+                                return result;
+                            }
+                        }
+
+                        System.out.println("Keywords were not found in the tweet: " + tweet);
+                        return result;
+                    }
+                });
     }
 }
